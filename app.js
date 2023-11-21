9390da135458e39148d0daab43bfa30091d3291a
Dominic Park <dominicpark@Dominics-Air.hsd1.wa.comcast.net>
2023-11-21 14:49:47 -0800

    update

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import Lab5 from "./lab5.js";
 import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
+import "dotenv/config";
 
 const app = express();
 app.use(cors());
@@ -13,4 +14,4 @@ ModuleRoutes(app);
 
 Lab5(app);
 Hello(app);
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
